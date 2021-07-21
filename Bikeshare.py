65450b7656f6b6f06da8fcbf23d63bf85a6119e2
Sinan <sinan043@live.de>
2021-07-21 16:17:37 +0200

    Restart text was updated

--- Bikeshare.py
+++ Bikeshare.py
@@ -197,7 +197,7 @@ def main():
         user_stats(df, city)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nDo you wish a restart of the Program? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
